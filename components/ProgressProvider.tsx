17741eb4d8f0f055ff7a7d74cff9ffab32e44dc3
fkrydev <fikri.afa289@gmail.com>
2023-07-26 17:57:18 +0800

    disable progress bar

--- components/ProgressProvider.tsx
+++ components/ProgressProvider.tsx
@@ -5,7 +5,7 @@ import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
 export default function ProgressProvider({ children }: { children: React.ReactNode }) {
     return <>
         {children}
-        <ProgressBar color='#000000' />
+        {/* <ProgressBar color='#000000' height="4px" options={{ showSpinner: false }} shallowRouting /> */}
     </>
 
 }
